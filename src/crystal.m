3bbf355e9d5a1a5e39d6a52675db476333294645
galois60 <pbrooksb@bucknell.edu>
2018-08-29 10:31:56 -0400

    Create crystal.m

---
+++ src/crystal.m
@@ -0,0 +1,78 @@
+/* 
+    INPUT:
+      (1) L, an irreducible representation of a simple Lie algebra.
+    
+    OPTIONAL:  
+      (2) E, the part of a Chevalley basis corresponding to the
+          positive fundamental roots of L. In particular, the 
+          length of E is the Lie rank r of L.
+      (3) F, the opposite fundamental roots.
+      
+    OUTPUT:
+      (1) a transition matrix to a crystal basis.
+      (2) a crystal basis data structure, which is a list whose
+          members are tuples with the following information:
+           * a vector v
+           * a label for v: a word in [ F[1] ... F[r] ]
+           * labelled pointers to v from previous basis elements
+*/ 
+intrinsic CrystalBasis (L::AlgMatLie : E := [] , F := []) -> 
+              AlgMatElt, SeqEnum
+  {Finds a transition matrix to a highest weight basis for L relative to E and F.}
+     
+     k := BaseRing (L);
+     n := Degree (L);
+     
+     if #E eq 0 then
+         E, F := ChevalleyBasis (L);  
+     end if;
+     NE := sub < L | E >;   
+     NENE := NE * NE;
+     NF := sub < L | F >;
+     NFNF := NF * NF;
+     r := Dimension (NE) - Dimension (NENE);
+     assert r eq (Dimension (NF) - Dimension (NFNF));
+     E := [ E[i] : i in [1..#E] | not E[i] in NENE ];
+     F := [ F[i] : i in [1..#F] | not F[i] in NFNF ];
+     assert ((#E eq r) and (#F eq r)); 
+     
+     // find unique highest weight vector corresponding to E
+     HW := &meet [ Nullspace (x) : x in E ];
+     require Dimension (HW) eq 1 : "there must be a unique highest weight vector";
+     lambda := HW.1;
+     
+     // spin up the basis using elements of F, keeping track of words as we go
+     V := VectorSpace (k, n);
+     B := [ V!lambda ];
+     A := [* < V!lambda , [ ] , [ ]  > *];
+     B_old := B;
+     while #B lt n do
+          P := { [i, j] : i in [1..#B_old], j in [1..r] |                     
+                              B_old[i] * F[j] ne 0 };
+          B_new := [ ];
+          for p in P do
+               b := B_old[p[1]] * F[p[2]];
+               if (b * E[p[2]] ne 0) and (b * E[p[2]] in sub < V | B_old[p[1]] >) 
+                  and not b in sub < V | B cat B_new > then
+                    Append (~B_new, b);   
+                    labs := [ [ i , j ] : i in [1..#B] , j in [1..r] | 
+                        (B[i] * F[j] ne 0) and (B[i] * F[j] in sub < V | b >) and
+                        (b * E[j] ne 0) and (b * E[j] in sub < V | B[i] >)
+                        ];
+                    labs cat:= [ [ i + #B, j ] : i in [1..#B_new] , j in [1..r] |
+                        (B_new[i] * F[j] ne 0) and (B_new[i] * F[j] in sub < V | b >) and
+                        (b * E[j] ne 0) and (b * E[j] in sub < V | B_new[i] >)
+                        ];
+                    w := Append (A[labs[1][1]][2], labs[1][2]);
+                    Append (~A, < b , w , labs >);
+               end if;
+          end for;
+          B cat:= B_new;
+          B_old := B_new;
+     end while;
+     
+     C := Matrix (B);
+     
+return C, A;
+
+end intrinsic;
