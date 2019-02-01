8ecb1b2969c9639a4547279ed3120fbfe088a5ae
Peter Brooksbank <pbrooksb@bucknell.edu>
2019-02-01 20:57:43 +0000

    fixed small bugs reported by EAO

--- src/Lie/normalizer.m
+++ src/Lie/normalizer.m
@@ -102,13 +102,19 @@ __AutosOfSimpleLie := function (J, E, F)
           pos +:= ni;
           
      end for;
+     
+     // sanity checks
      assert JC ^ delta eq JC;
      assert forall { g : g in GAMMA | JC ^ (GL(Nrows(g),k)!g) eq JC };
      
+     // conjugate back
      gens := [ delta ] cat 
              [ gamma : gamma in GAMMA | gamma ne Identity (MatrixAlgebra (k, n)) ];
      gens := [ C^-1 * gens[i] * C : i in [1..#gens] ];
+     
+     // assign the order of H
      H := sub < GL (n, k) | [ GL (n, k)!x : x in gens ] >;
+     order := FactoredOrder (H);
 
 return H;
 end function;
@@ -121,25 +127,38 @@ Used the same name as the function created (presumably) by Colva for groups.
       a subalgebra of gl(U_1) x ... x gl(U_m) in block diagonal form.)
   OUTPUT: the subgroup N(L) of GL(V) of elements normalizing L.
      (optional: the subgroup of GL(U_1) x ... x GL(U_m) normalizing L.)
+     
+  The "SANITY" and "ORDER" flags are temporary, while we figure out the correct
+  order formulae for the various components, and the orders of their intersections.
 */
-intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
+intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ], SANITY := false, ORDER := false) 
+          -> GrpMat
   { Returns the group of invertible matrices normalizing the matrix Lie algebra L. }
   
   flag, LL := HasLeviSubalgebra (L);
   vprint MatrixAlgebras, 1 : "verified that L is its own Levi subalgebra";
   require (flag and (L eq LL)) : 
      "at present the function works only for semisimple Lie algebras";
-  
-  k := BaseRing (L);
+   
+  k := BaseRing (L); 
   n := Degree (L);
-  G := GL (n, k);
-  V := VectorSpace (k, n);
   
   if #PARTITION eq 0 then
       PARTITION := [ n ];
   end if;
-  vprint MatrixAlgebras, 1 : "  [ GLNormalizer : PARTITION =", PARTITION, "]";
+     
+  // trivial case
+  if Dimension (L) eq 0 then    // everything normalizes L
+       NORM := GL (PARTITION[1], k); 
+       for i in [2..#PARTITION] do
+            NORM := DirectProduct (NORM, GL (PARTITION[i], k));
+       end for;
+       return NORM;
+  end if;
   
+  G := GL (n, k);
+  V := VectorSpace (k, n);
+    
   require (n eq &+ PARTITION) : "the specified partition is incompatible with the degree of L";
   
   require forall { i : i in [1..Ngens (L)] | __IsBlockDiagonalMatrix (Matrix (L.i), PARTITION) } :
@@ -148,11 +167,12 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
   // find a Chevalley basis for L and use it to exponentiate
   E, F := ChevalleyBasis (L);
   vprint MatrixAlgebras, 1 : "found a Chevalley basis for L";
-  H := sub < G | [ Exponentiate (z) : z in E cat F ] >;
+  EXP := sub < G | [ Exponentiate (z) : z in E cat F ] >;
   
   // compute the restrictions of L to the blocks determined by PARTITION,  
   // and the subgroup centralizing L within GL(U1) x ... x GL(Ut)
   CENTS := [ ];
+  SCALS := [ ];
   MPART := [* *];
   pos := 1;
   for i in [1..#PARTITION] do
@@ -164,16 +184,33 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
       Mi := RModule (Li);
       CentMi := EndomorphismAlgebra (Mi);  
       isit, Ci := UnitGroupOfAlgebra (CentMi);   assert isit;
+      ScalMi := CentreOfEndomorphismAlgebra (Mi);
+      isit, SCALi := UnitGroupOfAlgebra (ScalMi);   assert isit;
+      Append (~SCALS, SCALi);
       Append (~CENTS, Ci);
       pos +:= m;
   end for;
-  C := DirectProduct (CENTS);
-  assert forall { i : i in [1..Ngens (C)] | 
-                      forall { j : j in [1..Ngens (L)] | L.j ^ C.i eq L.j } };
-  ord := #H;
-  H := sub < G | H , C >;
-  vprint MatrixAlgebras, 1 : "  [ GLNormalizer: centralizer / exponential has order", #H div ord, "]";
-  vprint MatrixAlgebras, 2 : CompositionFactors (H);
+  CENT := DirectProduct (CENTS);
+  SCAL := DirectProduct (SCALS);
+  // compulsory sanity check 1
+  assert forall { i : i in [1..Ngens (CENT)] | 
+                      forall { j : j in [1..Ngens (L)] | L.j ^ CENT.i eq L.j } };
+  
+           // remove eventually
+           if ORDER then
+             CENT_ORDER := &* [ FactoredOrder (CENTS[i]) : i in [1..#CENTS] ];
+             ttt := Cputime ();
+             SCAL := LMGMeet (G, SCAL, EXP);
+             SCAL_ORDER := FactoredOrder (SCAL);
+             vprint MatrixAlgebras, 2 : "time to intersect full scalars with exponentiated group",
+             Cputime (ttt);
+             // optional SANITY check 1
+             if SANITY then
+                  INT := LMGMeet (G, EXP, CENT);
+                  flag := LMGEqual (INT, SCAL);
+                  vprint MatrixAlgebras, 2 : "   [SANITY CHECK 1]", flag;
+             end if;
+           end if;
   
   // get the minimal ideals of L and make sure they act "simply" on V     
   MI := IndecomposableSummands (L);
@@ -182,11 +219,11 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
   if #MI gt 1 then
       require forall { s : s in [1..#MI] |
             __AnnihilatesModule (MI[s], &+ [indV[t] : t in [1..#indV] | s ne t ]) } :
-"not all summands of L are irreducible J-modules for some minimal ideal J of L";
+"all L-module summands must be irreducible J-modules for some minimal ideal J of L (only in this restricted setting do the current methods work)";
   end if;
   IDIMS := [ [ Dimension (indV[i] meet MPART[j]) : j in [1..#MPART] ] : i in [1..#indV] ];
+  // compulsory sanity check 2
   assert forall { i : i in [1..#indV] | Dimension (indV[i]) eq &+ IDIMS[i] };
-  vprint MatrixAlgebras, 2 : "    intersection dimensions:", IDIMS;
   
   // put L into block diagonal form corresponding to the minimal ideals                    
   degs := [ Dimension (indV[i]) : i in [1..#indV] ];
@@ -198,12 +235,19 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
   FC := [ C * Matrix (F[i]) * C^-1 : i in [1..#F] ];
   
   // extract the blocks and construct the lifts of the outer automorphisms on each block
-  pos := 1;
   aut_gens := [ ];
   ISOTYPICS := [* *];
   SIMPLES := [ ];
   POSITIONS := [ ];
+              // remove eventually
+              if ORDER then
+                HAS_ORDER := true;
+                OUT_ORDER := Factorization (1);
+                EXP_ORDER := Factorization (1);
+              end if;
+  pos := 1;
   for s in [1..#MIC] do
+  
        Append (~POSITIONS, pos);
        Js := sub < MatrixLieAlgebra (k, degs[s]) |
             [ ExtractBlock ((MIC[s]).j, pos, pos, degs[s], degs[s]) : 
@@ -220,24 +264,70 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
             Append (~ISOTYPICS, <t, IDIMS[s], [s]>); 
        end if;                 
        LieRank := StringToInteger (&cat [t[i] : i in [2..#t]]);
+ 
+             // remove eventually: update the order of EXP
+             if ORDER then
+               if Characteristic (k) gt LieRank then  // we know the order of the exponentiated part
+                    EXP_ORDER *:= FactoredOrder (GroupOfLieType (t, k));
+               else
+                    HAS_ORDER := false;
+               end if;
+             end if;
+       
        ECs := [ ExtractBlock (EC[j], pos, pos, degs[s], degs[s]) : j in [1..#EC] ];
        FCs := [ ExtractBlock (FC[j], pos, pos, degs[s], degs[s]) : j in [1..#FC] ];
        ECs := [ e : e in ECs | e ne 0 ];
        FCs := [ f : f in FCs | f ne 0 ];
        OUTs := __AutosOfSimpleLie (Js, [ECs[i] : i in [1..LieRank]], [FCs[i] : 
                             i in [1..LieRank]]);
+                            
+              // remove eventually
+              if ORDER then
+                OUT_ORDER *:= FactoredOrder (OUTs);
+              end if;
+              
        aut_gens cat:= [ InsertBlock (Identity (G), OUTs.j, pos, pos) : 
                                                      j in [1..Ngens (OUTs)] ];
        pos +:= degs[s];
+       
   end for;
+  
+  NORM := sub < G | EXP , CENT >;
+  
+             // remove eventually
+             if ORDER then
+               if HAS_ORDER then
+                    EXP`FactoredOrder := EXP_ORDER; 
+       
+                    // optional SANITY check 2
+                    if SANITY then
+                         vprint MatrixAlgebras, 2 : "   [SANITY CHECK 2]", 
+                         FactoredOrder (NORM) eq EXP_ORDER * CENT_ORDER / SCAL_ORDER;
+                    end if;
+       
+                    // optional SANITY check 3
+                    if SANITY then
+                         AUT := sub < G | [ C^-1 * aut_gens[i] * C : i in [1..#aut_gens] ] >;
+                         IAN := LMGMeet (G, AUT, NORM);
+                         vprint MatrixAlgebras, 2 : "    [SANITY CHECK 3]", 
+                         FactoredOrder (IAN);
+                    end if;
+       
+                end if;
+              end if;
+  
   assert forall { g : g in aut_gens | LC ^ (GL (Nrows (g), k)!g) eq LC }; 
   vprint MatrixAlgebras, 2 : "    isotypic component data:", ISOTYPICS;
   
+  // compute the group permuting the isotypic components
   perm_gens := [ ];
+  PERM_ORDER := Factorization (1);
   ID := Identity (MatrixAlgebra (k, n));
   for s in [1..#ISOTYPICS] do
        S := ISOTYPICS[s][3];   // the simples to be permuted
        if #S gt 1 then
+           SYM_GENS := [ ];    // related to ORDER computation only
+           perm_gens := [ ];
            for i in [1..#S-1] do
                for j in [i+1..#S] do
                    // try to interchange simples S[i] and S[j]
@@ -245,6 +335,7 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
                    J := SIMPLES[S[j]];
                    isit, g := IsConjugate (I, J);
                    if isit then
+                       Append (~SYM_GENS, SymmetricGroup (#S)!(i,j));
                        h := ID;
                        InsertBlock (~h, MatrixAlgebra (k, degs[S[i]])!0, POSITIONS[S[i]], POSITIONS[S[i]]);
                        InsertBlock (~h, MatrixAlgebra (k, degs[S[j]])!0, POSITIONS[S[j]], POSITIONS[S[j]]);
@@ -254,28 +345,46 @@ intrinsic GLNormalizer (L::AlgMatLie : PARTITION := [ ]) -> GrpMat
                    end if;
                end for;
            end for;
+                     
+                     // remove eventually
+                     if ORDER then
+                       PERM_s := sub < SymmetricGroup (#S) | SYM_GENS >;
+                       PERM_ORDER *:= FactoredOrder (PERM_s);  
+                     end if;
+                            
        end if;
   end for;
+  
   assert forall { g : g in perm_gens | LC ^ (GL (Nrows (g), k)!g) eq LC };
   
-  // add in conjugated auto gens
-  aut_gens := [ C^-1 * aut_gens[i] * C : i in [1..#aut_gens] ];
-  ord := #H;
-  H := sub < G | aut_gens , H >;  
-  vprint MatrixAlgebras, 1 : "  [ GLNormalizer: algebra autos / cent-exp has order", #H div ord, "]";
-  vprint MatrixAlgebras, 2 : CompositionFactors (H);
+  NORM := sub < G | NORM , 
+                    [ C^-1 * aut_gens[i] * C : i in [1..#aut_gens] ] , 
+                    [ C^-1 * perm_gens[i] * C : i in [1..#perm_gens] ]
+              >;
   
-  // add in conjugated perm gens
-  perm_gens := [ C^-1 * perm_gens[i] * C : i in [1..#perm_gens] ];
-  ord := #H;
-  H := sub < G | H , perm_gens >;
-  vprint MatrixAlgebras, 1 : "  [ GLNormalizer: permutation autos / algebra autos has order", #H div ord;
-  vprint MatrixAlgebras, 2 : CompositionFactors (H);
+            // remove eventually
+            if ORDER then
+                 if HAS_ORDER then
+                    NORM`FactoredOrder := CENT_ORDER * EXP_ORDER * OUT_ORDER * PERM_ORDER / SCAL_ORDER;
+                    vprint MatrixAlgebras, 2 : "storing factored order";
+                 else
+                    vprint MatrixAlgebras, 2 : "could not store factored order";
+                 end if;
+                 // optional SANITY check 4
+                 if SANITY then
+                    H := sub < G | NORM >;
+                    assert not assigned H`FactoredOrder;
+                    vprint MatrixAlgebras, 2 : "   [SANITY CHECK 4]", 
+                          IntegerRing ()!FactoredOrder (NORM) eq LMGOrder (H);
+                    vprint MatrixAlgebras, 2 : "   |NORM| =", FactoredOrder (NORM);
+                    vprint MatrixAlgebras, 2 : "   |H| =", FactoredOrder (H);
+                 end if;
+             end if;
+
+  // final compulsory sanity check
+  assert forall { i : i in [1..Ngens (NORM)] | L ^ NORM.i eq L };  
   
-  // final sanity check
-  assert forall { i : i in [1..Ngens (H)] | L ^ H.i eq L };  
-  
-return H;
+return NORM;
 
 end intrinsic;
 
--- tests/LieNorm1.m
+++ tests/LieNorm1.m
@@ -1,5 +1,5 @@
 /* some functions to test the GLNormalizer function for Lie algebras */
-load "~/MagmaPackages/MatrixAlgebras/examples/Lie_constructs.m";
+load "~/MagmaGit/MatrixAlgebras/examples/Lie_constructs.m";
 
 /*
   A dinky example to illustrate the effect of PARTITION on the permutation
@@ -7,7 +7,7 @@ load "~/MagmaPackages/MatrixAlgebras/examples/Lie_constructs.m";
   within the domain / codomain specified by PARTITION.
 */
 k := GF (7);
-SetVerbose ("MatrixAlgebras", 1);
+SetVerbose ("MatrixAlgebras", 2);
 PART1 := [];
 PART2 := [2,2,2];
 ST := [ "A1" , "A1" , "A1" ];
@@ -28,11 +28,11 @@ assert #G1 div #G2 eq 6;
 */
 PART1 := [];
 PART2 := [2,2,2];
-ST := [ "A1" ];
+ST := [ "A2" ];
 RT := [ [3,0] ];
-L := MySemisimpleMatrixLieAlgebra (k, ST, RT : SCRAMBLE := false);
+L := MySemisimpleMatrixLieAlgebra (k, ST, RT : SCRAMBLE := true);
    // L is a diagonal embedding of sl(2,k) in gl(6,k)
-G1 := GLNormalizer (L : PARTITION := PART1);
+G1 := GLNormalizer (L : PARTITION := PART1, SANITY := true);
    // within G1 is the full centralizer of L in GL(6,k)
 G2 := GLNormalizer (L : PARTITION := PART2);
    // G2 contains only the centralizer on each domain / codomain space
